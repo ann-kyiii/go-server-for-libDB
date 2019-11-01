e012e7a748f22fd7cdf7546f98e7dd12391bfb1e
ataronus <im2tb4njto3zdcl@gmail.com>
2019-11-01 23:42:08 +0900

    impl API of searching Genre & SubGenre

--- main_server.go
+++ main_server.go
@@ -32,6 +32,8 @@ func initRouting(e *echo.Echo) {
 	e.GET("/", hello)
 	e.GET("/api/v1/bookId/:id", getBookWithID)
 	e.POST("/api/v1/search", searchBooks)
+	e.POST("/api/v1/searchGenre", searchGenre)
+	e.POST("/api/v1/searchSubGenre", searchSubGenre)
 }
 
 
@@ -128,4 +130,154 @@ func searchBooks(c echo.Context) error {
 	searchAttribute := []string{"publisher", "author", "bookName", "pubdate", "ISBN"}
 	data := searchOR(bookvalues, keywords, searchAttribute, offset, limit)
 	return c.JSON(http.StatusOK, data)
+}
+
+func searchGenre(c echo.Context) error {
+	m := echo.Map{}
+	if err := c.Bind(&m); err != nil {
+		return err
+	}
+	genre := m["genre"].(string)
+	t_offset := m["offset"].(string)
+	t_limit := m["limit"].(string)
+	offset, err := strconv.Atoi(t_offset)
+	if err != nil {
+		log.Printf("【Error】", err)
+		panic(err)
+	}
+	limit, err2 := strconv.Atoi(t_limit)
+	if err2 != nil {
+		log.Printf("【Error】", err)
+		panic(err)
+	}
+
+	// get DB data
+	client, ctx := ConnectDB()
+	defer client.Close()
+	collection := os.Getenv("LIBAPP_COLLECTION")
+
+	var books []BookFireStore
+	iter := client.Collection(collection).Where("exist", "==", "〇").Where("genre", "==", genre).Documents(ctx)
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Printf("【Error】", err)
+			panic(err)
+		}
+		var book BookFireStore
+		doc.DataTo(&book)
+		books = append(books, book)
+	}
+	iter = client.Collection(collection).Where("exist", "==", "一部発見").Where("genre", "==", genre).Documents(ctx)
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Printf("【Error】", err)
+			panic(err)
+		}
+		var book BookFireStore
+		doc.DataTo(&book)
+		books = append(books, book)
+	}
+
+	if len(books) <= offset {
+		empty_list := []interface{}{}
+		data := map[string]interface{}{
+			"books":empty_list,
+		}
+		return c.JSON(http.StatusOK, data)
+	} else {
+		first := offset
+		var last int
+		if len(books) < offset+limit {
+			last = len(books)
+		}else{
+			last = offset+limit
+		}
+		data := map[string]interface{}{
+			"books":books[first:last],
+		}
+		return c.JSON(http.StatusOK, data)
+	}
+}
+
+func searchSubGenre(c echo.Context) error {
+	m := echo.Map{}
+	if err := c.Bind(&m); err != nil {
+		return err
+	}
+	subGenre := m["subGenre"].(string)
+	t_offset := m["offset"].(string)
+	t_limit := m["limit"].(string)
+	offset, err := strconv.Atoi(t_offset)
+	if err != nil {
+		log.Printf("【Error】", err)
+		panic(err)
+	}
+	limit, err2 := strconv.Atoi(t_limit)
+	if err2 != nil {
+		log.Printf("【Error】", err)
+		panic(err)
+	}
+
+	// get DB data
+	client, ctx := ConnectDB()
+	defer client.Close()
+	collection := os.Getenv("LIBAPP_COLLECTION")
+
+	var books []BookFireStore
+	iter := client.Collection(collection).Where("exist", "==", "〇").Where("subGenre", "==", subGenre).Documents(ctx)
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Printf("【Error】", err)
+			panic(err)
+		}
+		var book BookFireStore
+		doc.DataTo(&book)
+		books = append(books, book)
+	}
+	iter = client.Collection(collection).Where("exist", "==", "一部発見").Where("subGenre", "==", subGenre).Documents(ctx)
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Printf("【Error】", err)
+			panic(err)
+		}
+		var book BookFireStore
+		doc.DataTo(&book)
+		books = append(books, book)
+	}
+
+	if len(books) <= offset {
+		empty_list := []interface{}{}
+		data := map[string]interface{}{
+			"books":empty_list,
+		}
+		return c.JSON(http.StatusOK, data)
+	} else {
+		first := offset
+		var last int
+		if len(books) < offset+limit {
+			last = len(books)
+		}else{
+			last = offset+limit
+		}
+		data := map[string]interface{}{
+			"books":books[first:last],
+		}
+		return c.JSON(http.StatusOK, data)
+	}
 }
